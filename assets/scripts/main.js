9eb5579c61277904c792d5ade39e61760b0b1f04
chris obrien <cro1979@gmail.com>
2015-07-02 16:51:29 -0400

    added css & js from roots

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,29 @@
                 if (typeof ga === 'function') {
                     Sage.common.gaTrackOutboundLinks();
                 }
+
+
+                // requires .videoWrapper style
+                // change static sized iframe video to responsive sized ( add checks to apply for any other than Youtube)
+                if ($("iframe[src^='http://www.youtube.com'], iframe[src^='https://www.youtube.com']").length) {
+                    $("iframe[src^='http://www.youtube.com'], iframe[src^='https://www.youtube.com']").removeAttr('height').removeAttr('width').wrap("<div class='videoWrapper'></div>");
+                }
+
+
+                // RESPONSIVE BREAKPOINT READER
+                Sage.breakpoint.refreshValue();
+                // UNCOMMENT THIS SECTION IF YOU WANT TO READ BREAKPOINT ON RESIZE
+                // REQUIRES CLASSES LOCATED IN: _common.scss
+                // $(window).resize(function () {
+                //     Sage.breakpoint.refreshValue();
+                //     //TEST VIEW BREAKPOINT
+                //     //console.log( Sage.breakpoint.value);
+                // }).resize();
+
+
+                // RESPONSIVE TABLE FIXER
+                // UNCOMMENT THIS SECTION IF YOU WANT RESPONSIVE TABLES
+                //Sage.responsiveTable();
             },
             finalize: function () {
                 // JavaScript to be fired on all pages, after page specific JS is fired
@@ -50,6 +73,49 @@
                         label: this.href
                     }, Sage.common.gaLogEvent);
                 });
+            }
+        },
+        responsiveTable: function () {
+            if ($('table').length) {
+                // just get the user created tables
+                $table = $('table').not('.crayon-table');
+
+                //first fix any tables without theads
+                $($table).each(function () {
+                    $hasHead = $('thead td, thead th', this).length;
+                    if (!$hasHead) {
+                        $(this).prepend('<thead></thead>').find("tr:first").prependTo($('thead', this));
+                    }
+                });
+
+                //second update tables to have data attrs
+                $($table).each(function () {
+                    $hasHead = $('thead td, thead th', this).length;
+                    $col_titles = [];
+
+                    if ($hasHead) {//make sure our current table has what we need to get started.
+                        // cache our column titles (include td for bad html)
+                        $(this).find('th, td').each(function () {
+                            $content = $(this).text() + ': ';
+                            $col_titles.push($content);
+                        });
+
+                        // add our column titles to data attrs on each tr>td
+                        $(this).find('tr').each(function () {
+                            $row = $(this);
+                            $row.children("td").each(function (key) {
+                                $(this).attr('data-label', $col_titles[key]);
+                            });
+                        });
+                    }
+                });
+            }
+        },
+        breakpoint : {
+            refreshValue : function () {
+                this.value = window.getComputedStyle(
+                    document.querySelector('body'), ':before'
+                ).getPropertyValue('content').replace(/['"]+/g, '');
             }
         }
     };
